e29454171259ecb22aa87b65a385b48acc40003c
Timothy Nelson <tnelson@enigmasupercomputing.com>
2019-07-08 23:31:08 -0500

    Got winbuild run_build and run_pack working.

--- src/SharedVersionInfo.cs
+++ src/SharedVersionInfo.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyVersion("0.0.325.0")]
+[assembly: System.Reflection.AssemblyVersion("0.0.329.0")]
 
 
